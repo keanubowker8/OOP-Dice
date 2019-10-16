9e0f0c434002a5f158e22dfeeb48a69a67ee94f6
keanubowker8 <keanu.bowker@umuzi.org>
2019-10-16 11:22:46 +0200

    third commit

--- dice.py
+++ dice.py
@@ -6,27 +6,26 @@ class Die():
     
     def __init__(self,sides, probabilities=None):
         self.sides = sides
-        if probabilities==None:
-            probabilities = [1 for i in range(self.sides)]    
-        if type(probabilities) == list:
-            assert len(probabilities) == sides
-            for number in probabilities:
-                if number < 0:
-                    raise ValueError('negative probabilities not allowed')
-            if sum(probabilities)<1:
-                raise ValueError('probability sum must be greater than 0')
         self.probabilities = probabilities
  
     
     
     def setProbabilities(self):
+        if self.probabilities==None:
+            self.probabilities = [1 for i in range(self.sides)]    
+        if type(self.probabilities) == list:
+            assert len(self.probabilities) == self.sides
+            for number in self.probabilities:
+                if number < 0:
+                    raise ValueError('negative probabilities not allowed')
+            if sum(self.probabilities)<1:
+                raise ValueError('probability sum must be greater than 0')
         numbers = []
         lst = self.probabilities
         for i in range(1,self.sides+1): 
             numbers.append(str(i))
         new = [numbers*lst for numbers,lst in zip(numbers,lst)]
         newlst = [b for i in new for b in i]
-        #print(newlst)
         return newlst
     
     
@@ -35,22 +34,15 @@ class Die():
     def roll(self):
         lst = self.setProbabilities()    
         random_number = random.choice(lst)
-        self.value = random_number
-    
-# lst = setProbabilities()    
-# random_word = random.choice(lst)
-# print(random_word)    
+        self.value = random_number   
         
             
         
 die6 =  Die(6)
 die6.roll()
+print(die6.value)
 
-# die20 =  Die(6,[1,1,2,1,5,2])
-# die20.roll()
 
-print(die6.value) # this would print a number between 1 and 6 inclusive
-#print(die20.value) # this would print a number between 1 and 20 inclusive       
 
 class DiceFactory():
     value = 0
@@ -63,6 +55,7 @@ class DiceFactory():
          self.value = random.randrange(1, self.sides+1)
          print(self.value)
          return self.value
+
 
 
 factory20 = DiceFactory(20)
--- dice_test.py
+++ dice_test.py
@@ -1,17 +1,33 @@
 from dice import*
+import random
+random.seed(9001)
+import pytest
+
 
 die6 =  Die(6,[0,0,0,0,0,6])
+die5 =  Die(6,[-1,0,0,0,0,6])
+die4 =  Die(6,[0,0,0,0,0,0])
 
 
 def test_setProbabilities():
 	assert die6.setProbabilities() == ['6', '6', '6', '6', '6', '6']
+	with pytest.raises(Exception):
+		assert die5.setProbabilities() == Exception('negative probabilities not allowed')
+		assert die4.setProbabilities() == Exception('probability sum must be greater than 0')
 
 
 def test_roll():
-	die6.value = 6
+	die6.roll()
+	assert die6.value == '6'
+	with pytest.raises(Exception):
+		die5.roll()
+		assert die6.value == Exception('negative probabilities not allowed')
+		die4.roll()
+		assert die6.value == Exception('probability sum must be greater than 0')
+
 
 factory20 = DiceFactory(20)
 
 def test_make_die():
-	assert factory20.make_die() == 9
+	assert factory20.make_die() == 10
 
